c686118ea310ebf60d6befb32d474be7e3a44411
Dew <cham970421@naver.com>
2024-02-06 12:34:28 +0900

    leeseul commit

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from datetime import datetime
 # base_path = 'home/'
 # Create your views here.
+#이슬 성공
 def index(request):
 
     return render(request,'home/base.html')
