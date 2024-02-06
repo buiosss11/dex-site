42f8a9c6295207f31b505c042b3b161e3f94290e
woohyun <buiosss11@naver.com>
2024-02-06 10:57:48 +0900

    upadte

--- home/views.py
+++ home/views.py
@@ -3,30 +3,5 @@ from datetime import datetime
 # base_path = 'home/'
 # Create your views here.
 def index(request):
-    # year  = datetime.today().year
-    # month  = datetime.today().month
-    # day  = datetime.today().day
-    # context = {
-    #     "year": year,
-    #     "month": month,
-    #     "day": day,
-    # }
-    # print(year)
     return render(request,'home/base.html')
 
-# def items(request,item):
-#     if item == 'news':
-#         templates_path = base_path+'news.html'
-#     elif item == 'dex':
-#         templates_path = base_path+'dex.html'
-#     elif item == 'rpa_docs':
-#         templates_path = base_path+'rpa.html'
-#     elif item == 'apps_docs':
-#         templates_path = base_path+'apps.html'
-#     elif item == 'bi_docs':
-#         templates_path = base_path+'bi.html'
-#     elif item == 'create_account':
-#         templates_path = base_path+'create_account.html'
-        
-    
-#     return render(request,templates_path)
